5f8e8303932f05d5713d939e73bf6eb9442cef8f
unknown <shuhao.zhang@mail.mcgill.ca>
2021-09-14 12:28:52 -0400

    import heart icons

--- Spacestagram/src/components/Like.js
+++ Spacestagram/src/components/Like.js
@@ -1,15 +1,16 @@
 import React from "react";
 //import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart} from '@fortawesome/free-solid-svg-icons'
-
+import { faHeart as heartSolid }  from '@fortawesome/free-solid-svg-icons' 
+import  { faHeart as heartHollow } from "@fortawesome/free-regular-svg-icons";
 function Like(){
   return(
     <div>
     <p>Like</p>
     <i className='fas fa-heart' />
-    <FontAwesomeIcon icon = {faHeart}/>
-    <i class="far fa-heart"></i>
+    <FontAwesomeIcon icon = {heartSolid}/>
+    <FontAwesomeIcon icon = {heartHollow}/>
+
     </div>
 )
 }

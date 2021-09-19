103ea391c60d30720dd7fe128766e3cc9ee6c3a9
unknown <shuhao.zhang@mail.mcgill.ca>
2021-09-19 19:04:29 -0400

    add image title, copy right date and refresh page features

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Layout from './components/Layout';
 import Like from './components/Like';
 
 function App() {
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
     <div>
     <Layout/>
     <HandleAPI/>
     <Like/>
+    <button onClick={refreshPage}>Click to reload!</button>
     </div>
   );
 }
--- src/components/HandleAPI.js
+++ src/components/HandleAPI.js
@@ -4,25 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function HandleAPI(){
     const key = 'mNDDUPC12NDZcw5tt3OMr4cNmDNrbGi5SrD5x1zn';
-    const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&count=1`;
-    const [image, setImage] = useState(null);
-
+    let url = `https://api.nasa.gov/planetary/apod?api_key=${key}&count=1`;
+    let [image, setImage] = useState(null);
     let content = null;
-
+    /*
+    let str = "";
+    let element = document.getElementById('dateInput');
+    if (element != null) {
+        str = element.value;
+    }
+    else {
+        str = null;
+    }
+    */
     useEffect(() =>{
       axios.get(url)
           .then(response => {
           setImage(response.data)
       })
-    }, [url]);
+    }, [url ]);
     
     if (image){
       content = <div className = 'search'>
-      <input />
+      <input type = "text" id ='dateInput' placeholder = "Search date yyyy-mm-dd" />
       <button id = 'search' className="btn btn-primary">Search</button>
-      <h1>
-      {image.title}
-      </h1>
+      <h3 className = 'date'>{image[0].title}</h3>
+      <h3 className = 'date'>Space Photo on {image[0].date}</h3>
+      <h5>@Copyright: {image[0].copyright}</h5>
       <div>
         <img
         src = {image[0].url}
